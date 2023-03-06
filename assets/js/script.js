6f1466c3c062128d1dc212bb3037eee5d2235142
Luciano Aguirre <luciano.aguirre@collins.com>
2023-03-05 22:10:26 -0500

    Added questions to javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["string", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question: "The condition in an if/else statement is enclosed with _______.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parenthesis",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _______.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["comma","curly brackets", "quotes", "parenthesis"],
+    answer: "quotes",
+  },  
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["Javascript","terminal/bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
+];
